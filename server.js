20eef07117dbe675f25a17c625afa8dfa2ce2b85
culritesh <culritesh@gmail.com>
2016-09-26 00:23:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get ('/article-one',function (req,res){
+res.send('Articl one requested and will be served here.');
+});
+
+app.get ('/article-two',function (req,res){
+res.send ('Artcile two requested and will be served here');
+});
+
+
+app.get ('/article-three',function (req,res){
+res.send ('Artcile three requested and will be served here');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
