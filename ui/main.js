1c848359cbf2cc9d496726afcdc2b20f93028811
culritesh <culritesh@gmail.com>
2016-10-01 16:26:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,21 @@
-var button = document.getelementbyid('counter');
+	var button = document.getElementById('counter');
 
-button.onclick = function (){
-	var request = XMLHTTPRequest;
-	
-	request.onreadystatechange = function(){
-	if (request.readystate == XMLHTTPRequest.done) 
-	 {
-	   if (request.status ==200)
-		{
-		  var counter;
-		  counter = request.responsetext;
-		  span.innerhtml= counter.tostring;
-		}	   
-	
-	 } 
-	
-	}
-request.open('Get','hhtp//culritesh.imad.hasura-app.io',true);
-request.send(null);
-};
+	button.onclick = function (){
+		var request = XMLHTTPRequest;
+		
+		request.onreadystatechange = function(){
+		if (request.readystate == XMLHTTPRequest.done) 
+		 {
+		   if (request.status ==200)
+			{
+			  var counter;
+			  counter = request.responsetext;
+			  span.innerhtml= counter.tostring;
+			}	   
+		
+		 } 
+		
+		}
+	request.open('Get','hhtp//culritesh.imad.hasura-app.io',true);
+	request.send(null);
+	};
